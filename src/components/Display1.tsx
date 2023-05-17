0cf2f59936c8df748ae875b0c25398a290f8ae40
Sergey <Zaikouski.sergey@gmail.com>
2023-05-17 15:59:45 +0300

    first commit

---
+++ src/components/Display1.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+export const Display =()=> {
+    return (
+        <div className={"windows"}>
+            <div className={'big-window'}>
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
+
+            </div>
+            <div className={'small-window'}></div>
+        </div>
+    )
+}
